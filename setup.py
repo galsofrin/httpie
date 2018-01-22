405a0f1d0834184cc46861470236ca7ce74e3a56
galsofrin <35656491+galsofrin@users.noreply.github.com>
2018-01-22 17:22:17 +0200

    removed all versions but 3.6

--- setup.py
+++ setup.py
@@ -91,13 +91,6 @@ setup(
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.1',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Environment :: Console',
         'Intended Audience :: Developers',
